6d917f3881d881026cd6b9a63701463607adc7a8
nno24 <nicolai.norseng@gmail.com>
2022-04-21 21:08:12 +0000

    Setup project initials for django

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pcks="django dj_database_url gunicorn dj3-cloudinary-storage psycopg"
+
+for p in $pcks
+do
+   echo "installing $p"
+   pip3 install $p
+done
+
+echo "creating requirements"
+pip3 freeze --local > requirements.txt
