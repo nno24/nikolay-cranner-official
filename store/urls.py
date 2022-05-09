8fa153b1d1a54afa54a127f6a510a0944b82ec22
nno24 <nicolai.norseng@gmail.com>
2022-05-09 10:46:14 +0000

    Started on the store details view, rendering basic info on store details when products is clicked for the the all products view

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.get_store, name='store')
+    path('', views.get_store, name='store'),
+    path('<store_id>', views.store_details, name='store_details'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import requests
 from .models import Product, Category
 
 
 # Create your views here.
 def get_store(request):
-
+    """A view to show all product"""
+    
     products = Product.objects.all()
     context = {
         'products': products
     }
     return render(request, 'store/store.html', context)
+
+def store_details(request, store_id):
+    """A view to show product details"""
+
+    product = get_object_or_404(Product, pk=store_id)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'store/store_details.html', context)
