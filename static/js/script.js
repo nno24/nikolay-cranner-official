5e9f8dc25a1ce776c97d4f46c23b66f48e98a30c
nno24 <nicolai.norseng@gmail.com>
2022-05-16 12:53:29 +0000

    added dropdown to mobile view

--- static/js/script.js
+++ static/js/script.js
@@ -29,9 +29,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems, {'dismissible': true, 'opacity': 0.65});
   });
 
- //Initialize dropdown for login / signup etc
+ //Initialize dropdown for login / signup etc Desktop/Tablet
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, {});
+});
+
+ //Initialize dropdown for login / signup etc Mobile
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger2');
   var instances = M.Dropdown.init(elems, {});
 });
 
