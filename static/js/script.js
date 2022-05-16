2390efcf64a23210a8dd393eb2847ef2150e58f0
nno24 <nicolai.norseng@gmail.com>
2022-05-16 06:36:50 +0000

    Added error, cancel and init methods to paypal integration

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
+//Functions and variables ---------------------------------
+
+function printValues(obj) {
+  for (var key in obj) {
+      if (typeof obj[key] === "object") {
+          console.log('key: ' + key )
+          printValues(obj[key]);   
+      } else {
+          console.log('key: ' + key )
+          console.log('val: ' + obj[key]);    
+      }
+  }
+}
+
+let grand_total = document.getElementById('grand-total').textContent;
+let user_id = document.getElementById('user_id').textContent;
+let order_id = document.getElementById('order_id').textContent;
+
+
+//Event listeners -----------------------------------------
+
 //Initialize mobile menu
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
@@ -10,12 +31,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems, {'dismissible': true, 'opacity': 0.65});
   });
 
+//Auto fill hidden form for submitting order to database...
+document.addEventListener('DOMContentLoaded', function autoFillOrderForm() {
+  document.getElementById('id_order_id').setAttribute('value', order_id );
+  document.getElementById('id_user_id').setAttribute('value', user_id );
+  document.getElementById('id_grand_total').setAttribute('value', grand_total );
+  console.log("updating form fields");
+});
 
-let grand_total = document.getElementById('grand-total').textContent;
-let user_id = document.getElementById('user_id').textContent;
-let order_id = document.getElementById('order_id').textContent;
+/*
+ document.addEventListener('DOMContentLoaded' && 'submit', function openModal() {
+    var instance = M.Modal.getInstance($('#add-to-basket'));
+    instance.open();
+});
+*/
 
-
+//Paypal API integration ------------------------------------
 const payPalButtons = paypal.Buttons({
 
   style: {
@@ -31,18 +62,33 @@ createOrder: function(data, actions) {
       amount: {
          
           value: grand_total
-      }
+      },
     }]
   }
+  console.log('The order payload is: ' + JSON.stringify(createOrderPayload, null, 4));
   return actions.order.create(createOrderPayload);
 },
 
+//On Initializastion 
+onInit: (data, actions) => {
+  if (grand_total == 0){
+    actions.disable();
+  }
+  else {
+    actions.enable();
+  }
+},
+
+//If order is cancelled
+onCancel: (data) => {
+  alert('Payment Cancelled');
+},
 // Finalize the transaction
 onApprove: (data, actions) => {
     const captureOrderHandler = (details) => {
         const payerName = details.payer.name.given_name;
         console.log('Transaction completed for: ' + payerName);
-        console.log('The details: ' + Object.keys(details));
+        console.log('The details: ' + JSON.stringify(details, null, 4));
 
         document.getElementById('download-0').removeAttribute('disabled');
         let transaction_date = document.getElementById('transaction_date').textContent;
@@ -58,6 +104,7 @@ onApprove: (data, actions) => {
   // handle unrecoverable errors
   onError: (err) => {
       console.error('An error prevented the buyer from checking out with PayPal');
+      alert('Payment failed');
   }
   });
 
@@ -69,20 +116,8 @@ onApprove: (data, actions) => {
     });
 
 
-/*
- document.addEventListener('DOMContentLoaded' && 'submit', function openModal() {
-    var instance = M.Modal.getInstance($('#add-to-basket'));
-    instance.open();
-});
-*/
 
-//Auto fill hidden form for submitting order to database...
-document.addEventListener('DOMContentLoaded', function autoFillOrderForm() {
-  document.getElementById('id_order_id').setAttribute('value', order_id );
-  document.getElementById('id_user_id').setAttribute('value', user_id );
-  document.getElementById('id_grand_total').setAttribute('value', grand_total );
-  console.log("updating form fields");
-});
+
 
     
 
