2b12fb99b4a96871ef55a7f46e4048589a86f5b4
nno24 <nicolai.norseng@gmail.com>
2022-05-18 13:06:05 +0000

    added correct host to allowed hosts

--- nc/settings.py
+++ nc/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["nc.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["nc.herokuapp.com", "localhost", "nikolaycranner.herokuapp.com"]
 
 # Application definition
 
