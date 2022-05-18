c6b7e373c7aeffe7b44cab45a9e8f40d4bc957ed
nno24 <nicolai.norseng@gmail.com>
2022-05-18 15:19:01 +0000

    enabled sending real emails

--- nc/settings.py
+++ nc/settings.py
@@ -102,7 +102,6 @@ AUTHENTICATION_BACKENDS = [
 
 #Allauth config
 SITE_ID = 1
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_EMAIL_REQUIRED=True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE=True
 ACCOUNT_AUTHENTICATION_METHOD='username_email'
@@ -187,3 +186,15 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CSRF_TRUSTED_ORIGINS = ["https://8000-nno24-nikolaycrannerof-e6xkwe8oz4k.ws-eu45.gitpod.io"]
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'nikolay.cranner@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
