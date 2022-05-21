f6c1c47d761bccf52a1af77bb411c208c7915257
nno24 <nicolai.norseng@gmail.com>
2022-05-21 10:53:06 +0000

    fixed deleting bag if guest

--- home/views.py
+++ home/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 import requests
 from store.models import Bag
+from store.views import get_session_user
 
 # Create your views here.
 def get_home(request):
 
     #Delete bag in database if it exists for the session user.
     try:
-        session_user = request.user.username
+        session_user = get_session_user(request)
         bag = get_object_or_404(Bag, bag_name=session_user)
         print('bag deleted: ', bag.bag_name)
         bag.delete()
