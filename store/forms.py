571f4137e6df7c009df9156f1f192164f7f48140
nno24 <nicolai.norseng@gmail.com>
2022-06-30 09:32:59 +0000

    Html validation: Fixed issues from w3 validation for store, newsletter, contact, and userprofile

--- store/forms.py
+++ store/forms.py
@@ -4,37 +4,31 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     order_id = forms.CharField(label='order_id', widget=forms.TextInput(attrs={
         'class': 'order_id',
-        'placeholder': 'order_id',
         'type': 'hidden',
         'value': ''
     }))
     order_items = forms.CharField(label='order_items', widget=forms.TextInput(attrs={
         'class': 'order_items',
-        'placeholder': 'order_items',
         'type': 'hidden',
         'value': ''
     }))
     user_id = forms.CharField(label='user_id', widget=forms.TextInput(attrs={
         'class': 'user_id',
-        'placeholder': 'user_id',
         'type': 'hidden',
         'value': ''
     }))
     grand_total = forms.DecimalField(label='grand_total', widget=forms.TextInput(attrs={
         'class': 'grand_total',
-        'placeholder': 'grand total',
         'type': 'hidden',
         'value': 0.00
     }))
     transaction_date = forms.DateField(label='transaction_date', widget=forms.DateInput(attrs={
         'class': 'transaction_date',
-        'placeholder': 'transaction_date',
         'type': 'hidden',
         'value': ''
     }))
     transaction_time = forms.TimeField(label='transaction_time', widget=forms.TimeInput(attrs={
         'class': 'transaction_time',
-        'placeholder': 'transaction_time',
         'type': 'hidden',
         'value': ''
     }))
